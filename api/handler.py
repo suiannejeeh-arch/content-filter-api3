a9e0c1cff335aa8068435bc68dd35943d146036b
suiannejeeh-arch <suianne.jeeh@gmail.com>
2025-12-12 21:52:11 -0300

    Delete api directory

--- api/handler.py
+++
@@ -1,5 +0,0 @@
-from vercel_asgi import VercelAsgi
-from api.index import app  # importa o app FastAPI definido no index.py
-
-# Cria o handler que a Vercel usa para executar o FastAPI
-handler = VercelAsgi(app)
